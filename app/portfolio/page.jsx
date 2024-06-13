cfedc9554f114fab7a7bfce9abbf37e9039ce8e5
StarCallMacProM2 <starcallmacprom2@StarCallMacProM2s-Laptop.local>
2024-06-13 22:04:33 +0300

    adding projects

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -11,51 +11,54 @@ const Portfolio = () => {
             <span className='title-bg'>WORKS</span>
             <h1>MY <span className='text-common'>PORTFOLIO</span></h1>
           </div>
-          <div className="wrapper">
-
+          <h1 className='text-center text-white'>FRONT-<span className='text-common'>END</span></h1>
+          <div className="boxWrapper">
             <div className="box">
-              <Image src={'/images/project.png'} width={300} height={200} />
+              <Image src={'/images/projects/e-commerce-1.jpg'} width={300} height={200} />
               <div className="overlay d-flex align-items-center justify-content-center ">
                 <InsertLinkOutlinedIcon />
-                <h4 className='position-absolute'>project 1</h4>
-              </div>
-            </div>
-            <div className="box">
-              <Image src={'/images/project.png'} width={300} height={200} />
-              <div className="overlay d-flex align-items-center justify-content-center ">
-                <InsertLinkOutlinedIcon />
-                <h4 className='position-absolute'>project 1</h4>
-              </div>
-            </div>
-            <div className="box">
-              <Image src={'/images/project.png'} width={300} height={200} />
-              <div className="overlay d-flex align-items-center justify-content-center ">
-                <InsertLinkOutlinedIcon />
-                <h4 className='position-absolute'>project 1</h4>
-              </div>
-            </div>
-            <div className="box">
-              <Image src={'/images/project.png'} width={300} height={200} />
-              <div className="overlay d-flex align-items-center justify-content-center ">
-                <InsertLinkOutlinedIcon />
-                <h4 className='position-absolute'>project 1</h4>
-              </div>
-            </div>
-            <div className="box">
-              <Image src={'/images/project.png'} width={300} height={200} />
-              <div className="overlay d-flex align-items-center justify-content-center ">
-                <InsertLinkOutlinedIcon />
-                <h4 className='position-absolute'>project 1</h4>
-              </div>
-            </div>
-            <div className="box">
-              <Image src={'/images/project.png'} width={300} height={200} />
-              <div className="overlay d-flex align-items-center justify-content-center ">
-                <InsertLinkOutlinedIcon />
-                <h4 className='position-absolute'>project 1</h4>
+                <h4 className='position-absolute'>Ecommerce Store UI</h4>
               </div>
             </div>
 
+            <div className="box">
+              <Image src={'/images/projects/e-commerce-dashboard-1.jpg'} width={300} height={200} />
+              <div className="overlay d-flex align-items-center justify-content-center ">
+                <InsertLinkOutlinedIcon />
+                <h4 className='position-absolute'>Ecommerce Store Admin Dashboard</h4>
+              </div>
+            </div>
+            <div className="box">
+              <Image src={'/images/projects/portfolio.jpg'} width={300} height={200} />
+              <div className="overlay d-flex align-items-center justify-content-center ">
+                <InsertLinkOutlinedIcon />
+                <h4 className='position-absolute'>Personal Portfolio</h4>
+              </div>
+            </div>
+            <div className="box">
+              <Image src={'/images/projects/management-system-3.jpg'} width={300} height={200} />
+              <div className="overlay d-flex align-items-center justify-content-center ">
+                <InsertLinkOutlinedIcon />
+                <h4 className='position-absolute'>Jewelry Factory Mangement System UI</h4>
+              </div>
+            </div>
+          </div>
+          <h1 className='text-center text-white'>BACK-<span className='text-common'>END</span></h1>
+          <div className="boxWrapper">
+            <div className="box">
+              <Image src={'/images/projects/jewelry.jpg'} width={300} height={200} />
+              <div className="overlay d-flex align-items-center justify-content-center ">
+                <InsertLinkOutlinedIcon />
+                <h4 className='position-absolute'>Jewelry Factory Mangement System API</h4>
+              </div>
+            </div>
+            <div className="box">
+              <Image src={'/images/projects/ecommerce-api.jpg'} width={300} height={200} />
+              <div className="overlay d-flex align-items-center justify-content-center ">
+                <InsertLinkOutlinedIcon />
+                <h4 className='position-absolute'>Ecommerce Store API</h4>
+              </div>
+            </div>
           </div>
         </div>
       </section>
